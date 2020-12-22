99293ccfbb5b3fea358d79a4edff8b3a54e8a2c4
dbierer <doug@unlikelysource.com>
2020-12-22 13:19:16 +0700

    2020-12-22 DB: Updating infrastructure

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '07eec5d251c9dc5c5fb2756b1fcd82a41e41e35f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '07eec5d251c9dc5c5fb2756b1fcd82a41e41e35f',
+    ),
+    'linuxforphp/linuxforcomposer' => 
+    array (
+      'pretty_version' => '2.0.9',
+      'version' => '2.0.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '271f9f68c41d58573dfc50a13d43871363faa81f',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e38888a75c070304ca5514197d4847a59a5c853f',
+    ),
+  ),
+);
